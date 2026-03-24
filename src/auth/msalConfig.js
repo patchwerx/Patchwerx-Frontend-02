82d62baf83e69316d35b41a3fd079d5d78854dfe
patchwerx <brandon@patchwerx.com>
2026-03-23 21:26:27 -0400

    same thing new test

--- src/auth/msalConfig.js
+++ src/auth/msalConfig.js
@@ -40,6 +40,17 @@ export const msalConfig = {
 };
 
 // Delegated Graph scopes (calendar automation). offline_access required for refresh_token.
+// export const loginRequest = {
+//   scopes: [
+//     "openid",
+//     "profile",
+//     "offline_access",
+//     "User.Read",
+//     "Calendars.Read",
+//     "Calendars.ReadWrite",
+//   ],
+// };
+
 export const loginRequest = {
   scopes: [
     "openid",
@@ -47,7 +58,6 @@ export const loginRequest = {
     "offline_access",
     "User.Read",
     "Calendars.Read",
-    "Calendars.ReadWrite",
   ],
 };
 
